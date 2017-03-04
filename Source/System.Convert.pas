fa27d900b30e2c70d1e4b0dc6a241e92ac86e27c
Maxim Sysoev <maks4a@gmail.com>
2017-03-04 21:43:19 +0200

    Метод TConvert.ToBase64String(inArray: TArray<Byte>): String;

---
+++ Source/System.Convert.pas
@@ -0,0 +1,22 @@
+unit System.Convert;
+
+interface
+
+uses
+  System.NetEncoding;
+
+Type
+  TConvert = Class
+    Class Function ToBase64String(inArray: TArray<Byte>): String;
+  End;
+
+implementation
+
+{ TConvert }
+
+class function TConvert.ToBase64String(inArray: TArray<Byte>): String;
+begin
+  Result := TNetEncoding.Base64.EncodeBytesToString(inArray);
+end;
+
+end.
---
+++ Test/System.Convert.Test.pas
@@ -0,0 +1,35 @@
+unit System.Convert.Test;
+
+interface
+
+uses
+  System.Convert,
+  System.SysUtils,
+  DUnitX.TestFramework;
+
+type
+
+  [TestFixture]
+  TTestConvert = class(TObject)
+  public
+    [Test]
+    procedure ToBase64String;
+  end;
+
+implementation
+
+{ TTestConvert }
+
+procedure TTestConvert.ToBase64String;
+var
+  bytes: TArray<Byte>;
+begin
+  bytes := TEncoding.UTF8.GetBytes('abcdef==');
+  Assert.AreEqual('YWJjZGVmPT0=', TConvert.ToBase64String(bytes));
+end;
+
+initialization
+
+TDUnitX.RegisterTestFixture(TTestConvert);
+
+end.
